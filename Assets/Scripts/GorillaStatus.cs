bcbea321d4a21a6511956864a12322989d054dcc
Peichao Yu <peichao.yu@gree.co.jp>
2014-10-21 18:51:51 +0900

    add gorilla status

---
+++ Assets/Scripts/GorillaStatus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GorillaStatus : MonoBehaviour {
+	public int stamina;
+	public int reviveRate;
+	public int speed;
+	public int climbStrength;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
