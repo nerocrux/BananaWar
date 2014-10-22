51e56231b93c818dab6736136bfe93da5e484614
Peichao Yu <peichao.yu@gree.co.jp>
2014-10-22 18:27:43 +0900

    add a simple stamina bar

--- Assets/Scripts/GorillaStatus.cs
+++ Assets/Scripts/GorillaStatus.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class GorillaStatus : MonoBehaviour {
+	// param
 	public int stamina;
 	public int reviveRate;
 	public int speed;
 	public int climbStrength;
+	// status 
+	public int isTired;
 
 	// Use this for initialization
 	void Start () {
